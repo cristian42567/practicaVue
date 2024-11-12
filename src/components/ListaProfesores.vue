dc7ee67737a08ce037ff005198f0233bfe33ef9e
Cristian Fernando <cristianfernandovilluendas@gmail.com>
2024-11-12 19:10:20 +0100

    terminamos sin estilos

--- src/components/ListaProfesores.vue
+++ src/components/ListaProfesores.vue
@@ -11,12 +11,19 @@
     </div>
     <div><label>DNI: </label><input type="text" v-model="profesor.dni" /></div>
     <div>
-      <label>Materias: </label><input type="text" v-model="grupoMaterias" /><button @click="agregarMateria()">Agregar materia</button>
+      <label>Materias: </label
+      ><input type="text" v-model="grupoMaterias" /><button
+        @click="agregarMateria()"
+      >
+        Agregar materia
+      </button>
     </div>
     <div>
       <td>
         <ul>
-          <li v-for="(elm, index) in profesor.materia" :key="index">{{ elm }}</li>
+          <li v-for="(elm, index) in profesor.materia" :key="index">
+            {{ elm }}
+          </li>
         </ul>
       </td>
     </div>
@@ -25,6 +32,34 @@
       ><input type="checkbox" v-model="profesor.documentacion" />
     </div>
     <button @click="agregarProfesor()">Agregar profesor</button>
+  </section>
+
+  <section>
+    <table border="1" width="70%">
+      <tr>
+        <th>Nombre:</th>
+        <th>Apellidos:</th>
+        <th>DNI:</th>
+        <th>Materias:</th>
+        <th>documentación:</th>
+      </tr>
+      <tr v-for="index in grupoProfesores" :key="index.dni">
+        <td>{{ index.nombre }}</td>
+        <td>{{ index.apellidos }}</td>
+        <td>{{ index.dni }}</td>
+        <td>
+          <ul>
+            <li v-for="(elm, indice) in index.materia" :key="indice">
+              {{ elm }}
+            </li>
+          </ul>
+        </td>
+        <td>
+          <p v-if="index.documentacion">Documentación entregada.</p>
+          <p v-else>Documentación sin entregar.</p>
+        </td>
+      </tr>
+    </table>
   </section>
 </template>
 
@@ -41,22 +76,45 @@ let profesor = ref({
 
 let grupoProfesores = ref([]);
 
-let grupoMaterias = ref('');
+let grupoMaterias = ref("");
 
 const agregarMateria = () => {
-  if (grupoMaterias.value == '') {
-    alert('Debes añadir una materia')
-  }else{
+  if (grupoMaterias.value == "") {
+    alert("Debes añadir una materia.");
+  } else {
     profesor.value.materia.push(grupoMaterias.value);
-    grupoMaterias.value = '';
+    grupoMaterias.value = "";
   }
-}
+};
 
-const agregarProfesor = () =>{
-  
-}
+const limpiarFormulario = () => {
+  profesor.value.nombre = "";
+  profesor.value.apellidos = "";
+  profesor.value.dni = "";
+  profesor.value.materia = [];
+  profesor.value.documentacion = false;
+};
 
-
+const agregarProfesor = () => {
+  if (
+    profesor.value.nombre == "" ||
+    profesor.value.apellidos == "" ||
+    profesor.value.dni == "" ||
+    profesor.value.materia.length === 0
+  ) {
+    alert("Debes rellenar todos los campos.");
+    limpiarFormulario();
+  } else {
+    grupoProfesores.value.push({
+      nombre: profesor.value.nombre,
+      apellidos: profesor.value.apellidos,
+      dni: profesor.value.dni,
+      materia: profesor.value.materia,
+      documentacion: profesor.value.documentacion,
+    });
+    limpiarFormulario();
+  }
+};
 </script>
 
 <style scoped></style>
